a4294035f712d933c070bdbb1d65154e68916816
ZhPolina <polina.zheleznicova@yandex.ru>
2021-03-08 13:37:35 +0300

    Решена проблема с чтением файла проекта.

---
+++ unit2.pas
@@ -0,0 +1,39 @@
+unit Unit2;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, LCLTranslator;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    Label1: TLabel;
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm2 }
+
+procedure TForm2.FormCreate(Sender: TObject);
+begin
+
+end;
+
+
+end.
+
